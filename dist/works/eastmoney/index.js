bbf7863d4c1bb257dd611432175e59f41a02bc81
beyondverage0908 <beyondaverage0908@gmail.com>
2022-07-13 13:12:55 +0800

    更新调整

--- dist/works/eastmoney/index.js
+++ dist/works/eastmoney/index.js
@@ -7,13 +7,13 @@ exports.initEastmoneyJob = void 0;
 const node_schedule_1 = __importDefault(require("node-schedule"));
 const hq_1 = require("./hq");
 const hangqing_1 = require("../../mongodb/eastmoney/hangqing");
-const date_1 = __importDefault(require("../../lib/date"));
+const dayjs_1 = __importDefault(require("dayjs"));
 const hqEstmoneyJob = () => {
     const rule = new node_schedule_1.default.RecurrenceRule();
     rule.dayOfWeek = [1, 2, 3, 4, 5];
     rule.hour = [9, 10, 11, 13, 14];
     rule.second = 30;
-    const today = date_1.default.format();
+    const today = (0, dayjs_1.default)(new Date()).format("YYYY-MM-DD");
     node_schedule_1.default.scheduleJob(rule, async () => {
         const hqList = await (0, hq_1.getHq)();
         (0, hangqing_1.saveHqData)(hqList, today);
--- src/works/eastmoney/index.ts
+++ src/works/eastmoney/index.ts
@@ -1,6 +1,7 @@
 import schedule from "node-schedule";
 import { getHq } from "./hq";
 import { saveHqData } from "@/mongodb/eastmoney/hangqing";
+import dayjs from "dayjs";
 import DateTool from "@/lib/date";
 
 const hqEstmoneyJob = () => {
@@ -8,7 +9,7 @@ const hqEstmoneyJob = () => {
   rule.dayOfWeek = [1, 2, 3, 4, 5];
   rule.hour = [9, 10, 11, 13, 14];
   rule.second = 30;
-  const today = DateTool.format();
+  const today = dayjs(new Date()).format("YYYY-MM-DD");
   schedule.scheduleJob(rule, async () => {
     const hqList = await getHq();
     saveHqData(hqList, today);
