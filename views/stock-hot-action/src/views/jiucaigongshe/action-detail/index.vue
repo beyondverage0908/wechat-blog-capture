80e8097f5fac8632df564d33f731d39e89588c75
pingjun <pingjun@eastmoney.com>
2022-07-14 17:03:30 +0800

    build for test

--- views/stock-hot-action/src/views/jiucaigongshe/action-detail/index.vue
+++ views/stock-hot-action/src/views/jiucaigongshe/action-detail/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<n-data-table striped :data="tableData" :columns="columns"/>
+  <n-data-table striped :data="tableData" :columns="columns" />
 </template>
 <script setup lang="ts">
 import { computed, onMounted, ref, h } from "vue";
@@ -11,76 +11,87 @@ import { DataTableColumn } from "naive-ui";
 
 const columns = computed((): DataTableColumn[] => [
   {
-    title: '#',
-    key: 'index',
+    title: "#",
+    key: "index",
     width: 60,
     render: (_, index) => {
-      return `${index + 1}`
-    }
+      return `${index + 1}`;
+    },
   },
-	{
-		title: "证券名称",
-		key: "name",
-		width: 100,
-	},
-	{
-		title: "证券代码",
-		key: "code",
-		width: 100,
-	},
-	{
-		title: "价格",
-		key: "price",
-		width: 100,
-	},
-	{
-		title: "百分比",
-		key: "percent",
-		width: 100,
-	},
-	{
-		title: "链接",
-		key: "descLink",
-		width: 360,
-		render(row) {
-			const descLink: string = row.descLink as string;
-			if (!descLink) return;
-			return h(
-				"a",
-				{
-					style: {
-						textDecoration: "underline",
-						cursor: "pointer",
-					},
-					onClick: () => {
-						window.open(descLink, "_blank");
-					},
-				},
-				descLink
-			);
-		},
-	},
-	{
-		title: "描述",
-		key: "desc",
-		ellipsis: {
-			tooltip: {
-				width: 600,
-			},
-		},
-	},
+  {
+    title: "证券名称",
+    key: "name",
+    width: 100,
+  },
+  {
+    title: "证券代码",
+    key: "code",
+    width: 100,
+  },
+  {
+    title: "价格",
+    key: "price",
+    width: 100,
+  },
+  {
+    title: "百分比",
+    key: "percent",
+    width: 100,
+  },
+  {
+    title: "链接",
+    key: "descLink",
+    width: 360,
+    render(row) {
+      const descLink: string = row.descLink as string;
+      if (!descLink) return;
+      return h(
+        "a",
+        {
+          style: {
+            textDecoration: "underline",
+            cursor: "pointer",
+          },
+          onClick: () => {
+            window.open(descLink, "_blank");
+          },
+        },
+        descLink
+      );
+    },
+  },
+  {
+    title: "描述",
+    key: "desc",
+    ellipsis: {
+      tooltip: {
+        width: 600,
+      },
+    },
+  },
 ]);
 const tableData = ref<Stock[]>([]);
 
 onMounted(() => {
-	const data: Category[] = JSON.parse(storage.getStorage(LocalCategoryRange));
-	const route = useRoute();
-	const { name } = route.query;
-	const findCategory = data.find((item) => item.category === name);
-	if (!findCategory) {
-		return;
-	}
-	tableData.value = findCategory.stocks;
+  const data: Category[] = JSON.parse(storage.getStorage(LocalCategoryRange));
+  const route = useRoute();
+  const { name } = route.query;
+  const findCategory = data.find((item) => item.category === name);
+  if (!findCategory) {
+    return;
+  }
+  const stocks = findCategory.stocks || [];
+  const map = new Map<string, Stock[]>();
+  stocks.forEach((stock) => {
+    const has = !!map.get(stock.code);
+    if (has) {
+      map.get(stock.code)?.push(stock);
+    } else {
+      map.set(stock.code, [stock]);
+    }
+  });
+  tableData.value = [...map.values()]
+    .sort((a, b) => b.length - a.length)
+    .flat();
 });
 </script>
-
