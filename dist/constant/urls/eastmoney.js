f6ddb1f2fc5ded7d578a7bce3b46fb68be7b3701
pingjun <pingjun@eastmoney.com>
2022-07-28 12:01:01 +0800

    update .gitignore

--- dist/constant/urls/eastmoney.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mainUrl = void 0;
-exports.mainUrl = "https://www.eastmoney.com/";
--- dist/mongodb/model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.t_ths_liangjia = exports.t_em_hq = exports.t_jcgs_category = exports.t_jcgs_stock = void 0;
-exports.t_jcgs_stock = "t_jcgs_stock";
-exports.t_jcgs_category = "t_jcgs_category";
-exports.t_em_hq = "t_em_hq";
-exports.t_ths_liangjia = "t_ths_liangjia";
